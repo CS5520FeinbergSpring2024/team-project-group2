620bc69a80f31a051210be990a929b807ac0daab
nannigeri <annigeri.n@northeastern.edu>
2024-03-20 15:38:18 -0400

    adding base files

---
+++ app/src/main/java/edu/northeastern/recipeasy/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.northeastern.recipeasy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
