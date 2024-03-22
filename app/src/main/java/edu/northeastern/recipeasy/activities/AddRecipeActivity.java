221824fe9dc38f36f9f78b73db1dba373dec612c
chloedipetrillo <dipetrillo.c@northeastern.edu>
2024-03-22 16:29:35 -0400

    general input layout

---
+++ app/src/main/java/edu/northeastern/recipeasy/activities/AddRecipeActivity.java
@@ -0,0 +1,16 @@
+package edu.northeastern.recipeasy.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.northeastern.recipeasy.R;
+
+public class AddRecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_recipe);
+    }
+}
--- app/src/main/java/edu/northeastern/recipeasy/activities/MainActivity.java
+++ app/src/main/java/edu/northeastern/recipeasy/activities/MainActivity.java
@@ -2,7 +2,11 @@ package edu.northeastern.recipeasy.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import edu.northeastern.recipeasy.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openAddRecipeActivity(View view){
+        Intent intent = new Intent(this, AddRecipeActivity.class);
+        startActivity(intent);
     }
 }
